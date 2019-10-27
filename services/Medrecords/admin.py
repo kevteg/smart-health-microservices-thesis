0b6123b071e9bedc8d7e98f96f485d41bad6d369
kevteg <kevteg05@gmail.com>
2019-10-27 15:33:56 -0400

    admin for med_records app

--- services/Medrecords/admin.py
+++ services/Medrecords/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from Medrecords.models import Record, Patient
 
-# Register your models here.
+
+@admin.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    pass
--- services/Medrecords/urls.py
+++ services/Medrecords/urls.py
@@ -1,11 +1,17 @@
 from Medrecords.views.graphql import Graph
+from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
 from graphene_django.views import GraphQLView
 from django.conf.urls.static import static
 from django.conf import settings
 
+
+admin.site.site_header = 'Med records - Admin'
+
+
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('graph/', Graph.as_view()),
     path('graphiql/', GraphQLView.as_view(graphiql=True))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
